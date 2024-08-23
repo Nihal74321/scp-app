05472d893ac031adf626c43a5d1a29f5111cb921
Nihal Dharmaraj <nihal.dharmaraj@gmail.com>
2024-08-23 17:08:30 +1200

    restructured and added test content

---
+++ src/components/card.jsx
@@ -0,0 +1,8 @@
+import "./styles/main-file.css"
+export default function Card() {
+    return(
+        <div className="card-container">
+            <p className="test">This is some text</p>
+        </div>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import Card from "./components/card";
+import Information from "./components/inf-container";
+import "./components/styles/extras.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Card />
+    </React.StrictMode>
+)
+
+
